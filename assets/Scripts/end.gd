1c2639b2caf610500d3279857032ab5908057003
KayVeeZ <kshitijvashisth@gmail.com>
2025-07-04 19:55:46 +0100

    [feature] added warp out on trophy touch

--- assets/Scripts/end.gd
+++ assets/Scripts/end.gd
@@ -1,6 +1,13 @@
 extends Area2D
-
+@export var warp_out: PackedScene
 @export var target_level : PackedScene
+
+func spawn_warp(player_position: Vector2) -> void:
+	var warp_node = warp_out.instantiate()
+	warp_node.global_position = player_position
+	get_parent().add_child(warp_node)
+	await get_tree().create_timer(0.3).timeout
+	warp_node.queue_free()
 
 func _ready() -> void:
 	add_to_group("trophies")
@@ -8,7 +15,10 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		GameManager.level_index += 1
-		#await get_tree().create_timer(1).timeout
+		$sfx_warp_in.play()
+		body.hide()
+		spawn_warp(body.global_position)
+		await get_tree().create_timer(0.3).timeout
 		call_deferred("_change_scene")
 
 func _change_scene() -> void:
